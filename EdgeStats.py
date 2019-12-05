9a2f84436de1f0ccb24e13b19ddf60d64f42cf98
strotman.7 <ryan.strotman@gmail.com>
2019-12-04 21:38:23 -0500

    a little refactoring

---
+++ EdgeStats.py
@@ -0,0 +1,31 @@
+
+import math
+
+class EdgeStats:
+    const = 1 #TODO chnage this
+    
+    def __init__(self,prob_of_chosen):
+        self.N = 0
+        self.W = 0
+        self.P = prob_of_chosen
+        self.Q = 0
+        
+    def addN(self):
+        self.N += 1
+    
+    '''
+    v is the probability of winning at the end of the roll out
+    '''
+    def updateW(self,v):
+        self.W += v
+    
+    def calculateQ(self):
+        self.Q =  self.W / self.N
+    
+    def Update(self,v):
+        self.addN()
+        self.updateW(v)
+        self.calculateQ()
+        
+    def CalculateQplusU(self,total_n):
+        return self.Q + EdgeStats.const * self.P * math.sqrt(total_n) / (1 + self.N)
---
+++ TestGameNN.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+class TestGameNN:
+    
+    def __init__(self):
+        return
+    
+    def GetProbabilities(self,board_rep):
+        #throw into NN
+        return np.array([.1,.1,.4,.4])
+    
+    def GetProbOfWinning(self,board_rep):
+        return .5
+    
+
