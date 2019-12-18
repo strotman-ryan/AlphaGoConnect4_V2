84abddeb035a39205810aeb2afabf3709c170857
strotman.7 <ryan.strotman@gmail.com>
2019-12-17 22:47:00 -0500

    changed tabs to spaces

--- Connect4GameState.py
+++ Connect4GameState.py
@@ -7,194 +7,194 @@ from AbstractGameState import AbstractGameState
 
 class Connect4GameState(AbstractGameState):
 
-	def __init__(self,rep_of_board):
-		self.board = rep_of_board
+    def __init__(self,rep_of_board):
+        self.board = rep_of_board
 
-		
-		
-	'''
-	return array of next possible states - impossible moves have placeholder of current state
-	return array of probabilities produced by NN and renormalized
-	'''
-	def GetNextStatesWithProbabilites(self, neuralNetworks):
-		probabilities = neuralNetworks.GetMoveProbabilites(self)
-		children = np.array([])
-		returnedProbabilities = np.array([])
-		validMoves = self.getPossibleMoves()
-		for move in validMoves:
-			newBoard = self.board.copy()
-			newGameState = Connect4GameState(newBoard)
-			newGameState.addPlayerMove(move)
-			children = np.append(children,newGameState)
-			returnedProbabilities = np.append(returnedProbabilities, probabilities[move])
-		returnedProbabilities = returnedProbabilities / returnedProbabilities.sum() #renormalize
-		return (children, returnedProbabilities)
-		
-	'''
-	returns true if end of game
-	'''
-	def IsTerminal(self):
-		return self.EvaluateBoard() != Result.NotFinished
-		
-	'''
+        
+        
+    '''
+    return array of next possible states - impossible moves have placeholder of current state
+    return array of probabilities produced by NN and renormalized
+    '''
+    def GetNextStatesWithProbabilites(self, neuralNetworks):
+        probabilities = neuralNetworks.GetMoveProbabilites(self)
+        children = np.array([])
+        returnedProbabilities = np.array([])
+        validMoves = self.getPossibleMoves()
+        for move in validMoves:
+            newBoard = self.board.copy()
+            newGameState = Connect4GameState(newBoard)
+            newGameState.addPlayerMove(move)
+            children = np.append(children,newGameState)
+            returnedProbabilities = np.append(returnedProbabilities, probabilities[move])
+        returnedProbabilities = returnedProbabilities / returnedProbabilities.sum() #renormalize
+        return (children, returnedProbabilities)
+        
+    '''
+    returns true if end of game
+    '''
+    def IsTerminal(self):
+        return self.EvaluateBoard() != Result.NotFinished
+        
+    '''
     determines if two games states are the same
     return boolean
     '''
-	def IsSameAs(self, gameState):
-		return np.array_equal(self.board, gameState.board)
-	
-	'''
-	returns what NN determines
-	if a terminal node returns the value with respect to whos turn it is
-	'''
-	def ProbabilityOfWinning(self,neuralNetworks):
-		if not self.IsTerminal():
-			return neuralNetworks.GetProbabilityOfWinning(self)
-		print("Error - looking at terminal node in ProbabilityOfWinning")	
-		
-	'''
-	return a value between [0,1] that determines the value of the end game with respct to current player
-	'''
-	def ValueOfWinner(self):
-		if not self.IsTerminal():
-			print("Error - looking at NON-terminal node in ValueOfWinner")
-		board_state = self.EvaluateBoard()
-		player = self.whosMove()
-		if (player == 1 and board_state == Result.Player1Win):
-			return 1
-		if (player == -1 and board_state == Result.Player2Win):
-			return 1
-		if (board_state == Result.Tie):
-			return .5
-		return 0
+    def IsSameAs(self, gameState):
+        return np.array_equal(self.board, gameState.board)
+    
+    '''
+    returns what NN determines
+    if a terminal node returns the value with respect to whos turn it is
+    '''
+    def ProbabilityOfWinning(self,neuralNetworks):
+        if not self.IsTerminal():
+            return neuralNetworks.GetProbabilityOfWinning(self)
+        print("Error - looking at terminal node in ProbabilityOfWinning")   
+        
+    '''
+    return a value between [0,1] that determines the value of the end game with respct to current player
+    '''
+    def ValueOfWinner(self):
+        if not self.IsTerminal():
+            print("Error - looking at NON-terminal node in ValueOfWinner")
+        board_state = self.EvaluateBoard()
+        player = self.whosMove()
+        if (player == 1 and board_state == Result.Player1Win):
+            return 1
+        if (player == -1 and board_state == Result.Player2Win):
+            return 1
+        if (board_state == Result.Tie):
+            return .5
+        return 0
 
 
 
-	'''
-	given the probilities of moves add in 0s where moves are impossible
-	'''
-	def CompletePi(self, arrayOfProbabilites):
-		possibleMoves = self.getPossibleMoves()
-		for move in range(0,7):
-			if move not in possibleMoves:
-				arrayOfProbabilites = np.insert(arrayOfProbabilites,move, 0)
-		return arrayOfProbabilites
-		
-	'''
-	returns 1 if its Player1's turn, -1 if its Player2's turn
-	Can be used to simply place the correct chip as well as determine current player
-	'''
-	def whosMove(self):
-		return 1 if self.board.sum() == 0 else -1
-		
-	def GetNextStateFromHuman(self):
-		self.printBoard()
-		column = int(input("Input Valid Move From 0-6: "))
-		newGameState = Connect4GameState(self.board.copy())
-		newGameState.addPlayerMove(column)
-		return newGameState
+    '''
+    given the probilities of moves add in 0s where moves are impossible
+    '''
+    def CompletePi(self, arrayOfProbabilites):
+        possibleMoves = self.getPossibleMoves()
+        for move in range(0,7):
+            if move not in possibleMoves:
+                arrayOfProbabilites = np.insert(arrayOfProbabilites,move, 0)
+        return arrayOfProbabilites
+        
+    '''
+    returns 1 if its Player1's turn, -1 if its Player2's turn
+    Can be used to simply place the correct chip as well as determine current player
+    '''
+    def whosMove(self):
+        return 1 if self.board.sum() == 0 else -1
+        
+    def GetNextStateFromHuman(self):
+        self.printBoard()
+        column = int(input("Input Valid Move From 0-6: "))
+        newGameState = Connect4GameState(self.board.copy())
+        newGameState.addPlayerMove(column)
+        return newGameState
 
-	def printBoard(self):
-		boardCopy = self.board.astype(int).astype(str)
-		boardCopy[boardCopy == '0'] = "_"
-		boardCopy[boardCopy == '1'] = "O"
-		boardCopy[boardCopy == '-1'] = "X"
-		for row in range(6):
-			print(''.join(boardCopy[row]))
-		#print(boardCopy)
-					
-	def checkVertical(self, result):
-		for column in range(7):
-			if result != Result.NotFinished:
-				break
-			for row in range(3):
-				if result != Result.NotFinished:
-					break
-				if self.board[row, column] != 0:
-					if self.board[row, column] == self.board[row+1, column] and self.board[row, column] == self.board[row+2, column] and self.board[row, column] == self.board[row+3, column]:
-						if self.board[row, column] == 1:
-							result = Result.Player1Win
-						else:
-							result = Result.Player2Win
-		return result
+    def printBoard(self):
+        boardCopy = self.board.astype(int).astype(str)
+        boardCopy[boardCopy == '0'] = "_"
+        boardCopy[boardCopy == '1'] = "O"
+        boardCopy[boardCopy == '-1'] = "X"
+        for row in range(6):
+            print(''.join(boardCopy[row]))
+        #print(boardCopy)
+                    
+    def checkVertical(self, result):
+        for column in range(7):
+            if result != Result.NotFinished:
+                break
+            for row in range(3):
+                if result != Result.NotFinished:
+                    break
+                if self.board[row, column] != 0:
+                    if self.board[row, column] == self.board[row+1, column] and self.board[row, column] == self.board[row+2, column] and self.board[row, column] == self.board[row+3, column]:
+                        if self.board[row, column] == 1:
+                            result = Result.Player1Win
+                        else:
+                            result = Result.Player2Win
+        return result
 
-	def checkHorizontal(self,result):
-		if result == Result.NotFinished:
-			for row in range(6):
-				if result != Result.NotFinished:
-					break
-				for column in range(4):
-					if result != Result.NotFinished:
-						break
-					if self.board[row, column] != 0:
-						if self.board[row, column] == self.board[row, column+1] and self.board[row, column] == self.board[row, column+2] and self.board[row, column] == self.board[row, column+3]:
-							if self.board[row, column] == 1:
-								result = Result.Player1Win
-							else:
-								result = Result.Player2Win
-		return result
+    def checkHorizontal(self,result):
+        if result == Result.NotFinished:
+            for row in range(6):
+                if result != Result.NotFinished:
+                    break
+                for column in range(4):
+                    if result != Result.NotFinished:
+                        break
+                    if self.board[row, column] != 0:
+                        if self.board[row, column] == self.board[row, column+1] and self.board[row, column] == self.board[row, column+2] and self.board[row, column] == self.board[row, column+3]:
+                            if self.board[row, column] == 1:
+                                result = Result.Player1Win
+                            else:
+                                result = Result.Player2Win
+        return result
 
-	def checkDiagonoalUpRight(self,result):
-		if result == Result.NotFinished:
-			for row in range(3):
-				if result != Result.NotFinished:
-					break
-				for column in range(4):
-					if result != Result.NotFinished:
-						break
-					if self.board[row, column] != 0:
-						if self.board[row, column] == self.board[row+1, column+1] and self.board[row, column] == self.board[row+2, column+2] and self.board[row, column] == self.board[row+3, column+3]:
-							if self.board[row, column] == 1:
-								result = Result.Player1Win
-							else:
-								result = Result.Player2Win
-		return result
+    def checkDiagonoalUpRight(self,result):
+        if result == Result.NotFinished:
+            for row in range(3):
+                if result != Result.NotFinished:
+                    break
+                for column in range(4):
+                    if result != Result.NotFinished:
+                        break
+                    if self.board[row, column] != 0:
+                        if self.board[row, column] == self.board[row+1, column+1] and self.board[row, column] == self.board[row+2, column+2] and self.board[row, column] == self.board[row+3, column+3]:
+                            if self.board[row, column] == 1:
+                                result = Result.Player1Win
+                            else:
+                                result = Result.Player2Win
+        return result
 
-	def checkDiagonalDownRight(self,result):
-		if result == Result.NotFinished:
-			for row in range(3,6):
-				if result != Result.NotFinished:
-					break
-				for column in range(4):
-					if result != Result.NotFinished:
-						break
-					if self.board[row, column] != 0:
-						if self.board[row, column] == self.board[row-1, column+1] and self.board[row, column] == self.board[row-2, column+2] and self.board[row, column] == self.board[row-3, column+3]:
-							if self.board[row, column] == 1:
-								result = Result.Player1Win
-							else:
-								result = Result.Player2Win
-		return result
+    def checkDiagonalDownRight(self,result):
+        if result == Result.NotFinished:
+            for row in range(3,6):
+                if result != Result.NotFinished:
+                    break
+                for column in range(4):
+                    if result != Result.NotFinished:
+                        break
+                    if self.board[row, column] != 0:
+                        if self.board[row, column] == self.board[row-1, column+1] and self.board[row, column] == self.board[row-2, column+2] and self.board[row, column] == self.board[row-3, column+3]:
+                            if self.board[row, column] == 1:
+                                result = Result.Player1Win
+                            else:
+                                result = Result.Player2Win
+        return result
 
-	def checkTie(self, result):
-		if result == Result.NotFinished and self.isBoardFilled():
-			result = Result.Tie
-		return result
+    def checkTie(self, result):
+        if result == Result.NotFinished and self.isBoardFilled():
+            result = Result.Tie
+        return result
 
-	#returns an enum from Result based on board state
-	def EvaluateBoard(self):
-		result = Result.NotFinished;
-		result = self.checkVertical(result)
-		result = self.checkHorizontal(result)
-		result = self.checkDiagonoalUpRight(result)
-		result = self.checkDiagonalDownRight(result)
-		result = self.checkTie(result)
-		return result
-	
-	#returns a set of columns that have at least one open spot
-	def getPossibleMoves(self):
-		moves = np.array([], dtype = int)
-		for column in range(7):
-			if (self.board[:,column] == 0).sum() > 0:
-				moves = np.append(moves,column)
-		return moves
-		
-	#Update the board 
-	#Assume the move is legal
-	def addPlayerMove(self,column):
-		row = np.max(np.where(self.board[:,column] == 0))
-		self.board[row,column] = self.whosMove()
-		
-	def isBoardFilled(self):
-		return (self.board == 0).sum() == 0
+    #returns an enum from Result based on board state
+    def EvaluateBoard(self):
+        result = Result.NotFinished;
+        result = self.checkVertical(result)
+        result = self.checkHorizontal(result)
+        result = self.checkDiagonoalUpRight(result)
+        result = self.checkDiagonalDownRight(result)
+        result = self.checkTie(result)
+        return result
+    
+    #returns a set of columns that have at least one open spot
+    def getPossibleMoves(self):
+        moves = np.array([], dtype = int)
+        for column in range(7):
+            if (self.board[:,column] == 0).sum() > 0:
+                moves = np.append(moves,column)
+        return moves
+        
+    #Update the board 
+    #Assume the move is legal
+    def addPlayerMove(self,column):
+        row = np.max(np.where(self.board[:,column] == 0))
+        self.board[row,column] = self.whosMove()
+        
+    def isBoardFilled(self):
+        return (self.board == 0).sum() == 0
     
--- Connect4NN.py
+++ Connect4NN.py
@@ -7,97 +7,97 @@ from AbstractNeuralNetwork import AbstractNeuralNetwork
 
 class Connect4NN(AbstractNeuralNetwork):
 
-	def __init__(self):
-		self.winNN = Connect4WinNN()
-		self.movesNN = Connect4MoveNN()
+    def __init__(self):
+        self.winNN = Connect4WinNN()
+        self.movesNN = Connect4MoveNN()
 
-	def GetMoveProbabilites(self, gameState):
-		board_rep = gameState.board.copy()
-		transformedRep = self.transformBoard(board_rep)
-		return self.movesNN.Predict(np.array([transformedRep]))
+    def GetMoveProbabilites(self, gameState):
+        board_rep = gameState.board.copy()
+        transformedRep = self.transformBoard(board_rep)
+        return self.movesNN.Predict(np.array([transformedRep]))
 
-	def GetProbabilityOfWinning(self, gameState):
-		board_rep = gameState.board.copy()
-		transformedRep = self.transformBoard(board_rep)
-		return self.winNN.Predict(np.array([transformedRep]))
+    def GetProbabilityOfWinning(self, gameState):
+        board_rep = gameState.board.copy()
+        transformedRep = self.transformBoard(board_rep)
+        return self.winNN.Predict(np.array([transformedRep]))
 
-	def Train(self, trainingExamples):
-		results = []
-		pis = []
-		boards = []
-		for example in trainingExamples:
-			results.append(example.result)
-			pis.append(example.pi)
-			board_rep = example.gameState.board
-			boards.append(self.transformBoard(board_rep))
-		pis = np.array(pis) 
-		boards = np.array(boards)
-		self.winNN.Train(boards, results)
-		self.movesNN.Train(boards, pis)
+    def Train(self, trainingExamples):
+        results = []
+        pis = []
+        boards = []
+        for example in trainingExamples:
+            results.append(example.result)
+            pis.append(example.pi)
+            board_rep = example.gameState.board
+            boards.append(self.transformBoard(board_rep))
+        pis = np.array(pis) 
+        boards = np.array(boards)
+        self.winNN.Train(boards, results)
+        self.movesNN.Train(boards, pis)
 
-	def Save(self, fileName):
-		self.winNN.Save(fileName + "_policy")
-		self.movesNN.Save(fileName + "_moves")
+    def Save(self, fileName):
+        self.winNN.Save(fileName + "_policy")
+        self.movesNN.Save(fileName + "_moves")
 
-	def Load(self, fileName):
-		self.winNN.Load(fileName + "_policy")
-		self.movesNN.Load(fileName + "_moves")
-		
-	def transformBoard(self, board_rep):
-		board_rep1 = np.reshape(board_rep.copy(), 42)
-		board_rep2 = np.reshape(board_rep.copy(), 42)
-		board_rep1[board_rep1 != 1] = 0
-		board_rep2[board_rep2 != -1] = 0
-		return np.append(board_rep1, board_rep2)
-		
-		
+    def Load(self, fileName):
+        self.winNN.Load(fileName + "_policy")
+        self.movesNN.Load(fileName + "_moves")
+        
+    def transformBoard(self, board_rep):
+        board_rep1 = np.reshape(board_rep.copy(), 42)
+        board_rep2 = np.reshape(board_rep.copy(), 42)
+        board_rep1[board_rep1 != 1] = 0
+        board_rep2[board_rep2 != -1] = 0
+        return np.append(board_rep1, board_rep2)
+        
+        
 class Connect4WinNN:
 
-	def __init__(self):
-		self.model = Sequential()
-		self.model.add(Dense(units = 100, activation = 'relu', input_dim = 84))
-		self.model.add(Dense(units = 20, activation = 'relu'))
-		self.model.add(Dense(units = 1, activation = 'sigmoid'))
-		self.model.compile(loss = 'mean_squared_error', optimizer = 'sgd', metrics = ['accuracy'])
-	def Predict(self, board_rep):
-		return self.model.predict(board_rep)[0][0]
-		
-	def Train(self, board_reps, outcomes):
-		self.model.fit(board_reps, outcomes, epochs = 100, batch_size = 50)
-		
-	def Save(self, fileName):
-		fileName = fileName + '.h5'
-		self.model.save(fileName)
-	def Load(self, fileName):
-		fileName = fileName + '.h5'
-		self.model = load_model(fileName)
-		
+    def __init__(self):
+        self.model = Sequential()
+        self.model.add(Dense(units = 100, activation = 'relu', input_dim = 84))
+        self.model.add(Dense(units = 20, activation = 'relu'))
+        self.model.add(Dense(units = 1, activation = 'sigmoid'))
+        self.model.compile(loss = 'mean_squared_error', optimizer = 'sgd', metrics = ['accuracy'])
+    def Predict(self, board_rep):
+        return self.model.predict(board_rep)[0][0]
+        
+    def Train(self, board_reps, outcomes):
+        self.model.fit(board_reps, outcomes, epochs = 100, batch_size = 50)
+        
+    def Save(self, fileName):
+        fileName = fileName + '.h5'
+        self.model.save(fileName)
+    def Load(self, fileName):
+        fileName = fileName + '.h5'
+        self.model = load_model(fileName)
+        
 class Connect4MoveNN:
 
-	def __init__(self):
-		self.model = Sequential()
-		self.model.add(Dense(units = 100, activation = 'relu', input_dim = 84))
-		#self.model.add(Dense(units = 70,activation = 'relu'))  #added this line
-		self.model.add(Dense(units = 20,activation = 'relu'))  
-		self.model.add(Dense(units = 7, activation = 'softmax'))  
-		self.model.compile(loss = 'categorical_crossentropy', optimizer = 'sgd', metrics = ['accuracy'])
-	def Predict(self, board_rep):
-		return self.model.predict(board_rep)[0]
-		
-	def Train(self, board_reps, pis):
-		self.model.fit(board_reps, pis, epochs = 100, batch_size = 50)
-		
-	def Save(self, fileName):
-		fileName = fileName + '.h5'
-		self.model.save(fileName)
+    def __init__(self):
+        self.model = Sequential()
+        self.model.add(Dense(units = 100, activation = 'relu', input_dim = 84))
+        #self.model.add(Dense(units = 70,activation = 'relu'))  #added this line
+        self.model.add(Dense(units = 20,activation = 'relu'))  
+        self.model.add(Dense(units = 7, activation = 'softmax'))  
+        self.model.compile(loss = 'categorical_crossentropy', optimizer = 'sgd', metrics = ['accuracy'])
+    def Predict(self, board_rep):
+        return self.model.predict(board_rep)[0]
+        
+    def Train(self, board_reps, pis):
+        self.model.fit(board_reps, pis, epochs = 100, batch_size = 50)
+        
+    def Save(self, fileName):
+        fileName = fileName + '.h5'
+        self.model.save(fileName)
 
-	def Load(self, fileName):
-		fileName = fileName + '.h5'
-		self.model = load_model(fileName)
+    def Load(self, fileName):
+        fileName = fileName + '.h5'
+        self.model = load_model(fileName)
         
 
 
 
 
 
-		
+        
